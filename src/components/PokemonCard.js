09ceec2b81c7d92ae1faa02c8700406f5b5aae4f
rob1694 <robertb1694@gmail.com>
2021-05-30 22:12:54 -0400

    done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,23 +1,34 @@
 import React from "react";
 import { Card } from "semantic-ui-react";
+import { useState } from "react"
 
-function PokemonCard() {
+function PokemonCard({ pokemon, deletePokemon }) {
+  const [showFront, setShowFront] = useState(true);
+
+  const { name, hp, sprites } = pokemon,
+  { front, back } = sprites
+
+  function handleClick() {
+    setShowFront((showFront) => !showFront);
+  }
+
   return (
     <Card>
-      <div>
+      <div onClick = {handleClick}>
         <div className="image">
-          <img alt="oh no!" />
+          <img alt = {name} src = {showFront ? front : back }/>
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {hp}
           </span>
         </div>
       </div>
+        <button onClick = {deletePokemon}>Delete</button>
     </Card>
   );
 }
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -3,17 +3,40 @@ import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
+import { useEffect, useState } from "react"
+
+
 
 function PokemonPage() {
+
+  const URL = 'http://localhost:3001/pokemon'
+  const [pokemons, setPokemons] = useState([])
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    fetch(URL)
+      .then((r) => r.json())
+      .then((pokemons) => {
+        setPokemons(pokemons);
+      });
+  }, []);
+  function handleAddPokemon(newPokemon) {
+    setPokemons([...pokemons, newPokemon]);
+  }
+
+  const pokemonsToDisplay = pokemons.filter((pokemon) =>
+  pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm onAddPokemon = {handleAddPokemon}/>
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onChangeSearch={setSearchTerm} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemons={pokemonsToDisplay} />
     </Container>
   );
 }
